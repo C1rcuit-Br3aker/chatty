469f82229b368dd29f6fa75586bcb89474158eee
Chris Boone <cboone1337@gmail.com>
2016-04-25 09:43:21 -0500

    room updates messages and auto scrolls

--- app/room/route.js
+++ app/room/route.js
@@ -1,18 +1,7 @@
 import Ember from 'ember';
-import { task, timeout } from 'ember-concurrency';
 
 export default Ember.Route.extend({
   model() {
     return this.store.findAll(`message`);
   },
-
-
-  // setupController() {
-  //   this.get(`refreshPage`);
-  // },
-  //
-  // refreshPage: task(function * () {
-  //   yield timeout(2000);
-  //   return this.store.findAll(`message`);
-  // }),
 });
